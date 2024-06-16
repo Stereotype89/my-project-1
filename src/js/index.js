df960d871884cadc0ec3c9dcd70f9add0f5ff83c
OleksanderShyshman <sh.alexandr.1989@gmail.com>
2024-06-16 11:50:05 +0200

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,54 @@
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 
-const mainWrap = document.querySelector('#wrap');
-const sectionFirst = document.createElement('div');
-sectionFirst.classList.add('section');
-const sectionNext = document.createElement('div');
-sectionNext.classList.add('section');
+const mainWrap = document.querySelector("#wrap");
+const sectionFirst = document.createElement("div");
+sectionFirst.classList.add("section");
+const sectionNext = document.createElement("div");
+sectionNext.classList.add("section");
 
 mainWrap.prepend(sectionFirst, sectionNext);
 
-const labelMain = document.createElement('label');
-labelMain.classList.add('expression');
+const labelMain = document.createElement("label");
+labelMain.classList.add("expression");
 labelMain.textContent = `Натисни - 'Старт'`;
 
-const createBtnStart = document.createElement('button');
-createBtnStart.textContent = 'СТАРТ';
-createBtnStart.classList.add('start');
-createBtnStart.setAttribute('type', 'button');
+const createBtnStart = document.createElement("button");
+createBtnStart.textContent = "СТАРТ";
+createBtnStart.classList.add("start");
+createBtnStart.setAttribute("type", "button");
 
-const createBtnCheck = document.createElement('button');
-createBtnCheck.textContent = 'ПЕРЕВІРИТИ';
-createBtnCheck.classList.add('check');
-createBtnCheck.setAttribute('type', 'button');
+const createBtnCheck = document.createElement("button");
+createBtnCheck.textContent = "ПЕРЕВІРИТИ";
+createBtnCheck.classList.add("check");
+createBtnCheck.setAttribute("type", "button");
 
-const createInput = document.createElement('input');
-createInput.classList.add('item');
-createInput.setAttribute('placeholder', 'Введи відповідь');
+const createInput = document.createElement("input");
+createInput.classList.add("item");
+createInput.setAttribute("placeholder", "Введи відповідь");
 
 sectionFirst.prepend(labelMain, createBtnStart);
 sectionNext.prepend(createInput, createBtnCheck);
 
-const label = document.querySelector('.expression');
-const btnStart = document.querySelector('.start');
-const btnCheck = document.querySelector('.check');
-const input = document.querySelector('.item');
+const label = document.querySelector(".expression");
+const btnStart = document.querySelector(".start");
+const btnCheck = document.querySelector(".check");
+const input = document.querySelector(".item");
 
 let valueOfInput = 0;
 
-const onType = event => {
+const onType = (event) => {
   valueOfInput = Number(event.currentTarget.value);
   event.preventDefault();
 };
 
-input.addEventListener('input', onType);
+input.addEventListener("input", onType);
 
 function createItems() {
   const op = Math.floor(Math.random() * 2);
-  const x = ['+', '-'];
+  const x = ["+", "-"];
   const a = Math.floor(Math.random() * 100);
   const b = Math.floor(Math.random() * 100);
-  const plus = x[op] === '+';
+  const plus = x[op] === "+";
   return { op, x, a, b, plus };
 }
 
@@ -70,17 +70,17 @@ function checkExpression({ op, x, a, b, plus }) {
 let aaa;
 let check;
 
-btnStart.addEventListener('click', event => {
+btnStart.addEventListener("click", (event) => {
   aaa = createItems();
   check = checkExpression({ ...aaa });
 });
 
-btnCheck.addEventListener('click', event => {
-  if (valueOfInput === check) {
-    Notiflix.Report.success('МОЛОДЕЦЬ !!', 'Відповідь вірна! Натисни знову кнопку "СТАРТ"', 'OK');
-  } else if (valueOfInput === 0) {
-    Notiflix.Report.warning('УВАГА !!', 'Введи свою відповідь в поле! ;-)', 'OK');
-  } else {
-    Notiflix.Report.failure('УВАГА !!', 'Перевір правильність відповіді! :))', 'OK');
-  }
-});
+// btnCheck.addEventListener('click', event => {
+//   if (valueOfInput === check) {
+//     Notiflix.Report.success('МОЛОДЕЦЬ !!', 'Відповідь вірна! Натисни знову кнопку "СТАРТ"', 'OK');
+//   } else if (valueOfInput === 0) {
+//     Notiflix.Report.warning('УВАГА !!', 'Введи свою відповідь в поле! ;-)', 'OK');
+//   } else {
+//     Notiflix.Report.failure('УВАГА !!', 'Перевір правильність відповіді! :))', 'OK');
+//   }
+// });
